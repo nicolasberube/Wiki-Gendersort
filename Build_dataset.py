d349da8df8afaa7067b01287994320475d1c9f8e
nicolasberube <nicolasberube@gmail.com>
2020-06-15 18:57:57 -0400

    New code for gender assignation

--- Build_dataset.py
+++ Build_dataset.py
@@ -4,23 +4,13 @@
 @author: Nicolas Berube, 2016-2020
 for Vincent Larivière, EBSI, University of Montreal
 
-This code takes a list of first names separated by a line break \n in a .txt
-file (Names.txt) and constructs a database (NamesOut.txt) by assigning them
-a gender. NamesOut.txt will contain the same names, but followed by the
-assigned gender (both of them being seprated by a tab \t).
+Library to build the Wiki-Gendersort dataset, and to use it.
 
-The gender attribution (M, F, UNI, INI or UNK) is done according to the
-occurrence of first names on Wikipedia pages concerning them.
-M: male
-F: female
-UNI: unisex
-INI: initials
-UNK: unknown
+Use build_dataset() to build the dataset from scratch with Wikiedia searches.
+This should already been done on our own first name database and available
+in NamesOut.txt
 
-If NamesOut.txt already exists, it will be ignored and overwritten.
-Information on gender assignment is present in the log file (NamesLog.txt).
-The log file is automatically detected to launch the code back where it was
-in the case it got interrupted.
+Use wiki_gendersort() to assign a gender based on the built dataset.
 """
 
 from os.path import isfile
@@ -62,6 +52,14 @@ def index(a, x):
         return(i)
     else:
         return(-1)
+
+
+def file_len(fname, encod):
+    "Returns the number of lines in a files"
+    with open(fname, encoding=encod) as f:
+        for i, l in enumerate(f):
+            pass
+    return(i+1)
 
 
 def countalpha(name):
@@ -127,7 +125,6 @@ def lectdatalog(cwd, backup=True):
                         datalog.insert(name_idx, [name, gend, time, d])
         if not backup:
             remove(bu_name)
-
 
     return datalog, datanames
 
@@ -285,9 +282,31 @@ def name_to_gender(name):
     return gender, log_data
 
 
-if __name__ == '__main__':
-    # Set True if you want to disregard log files
-    reboot = False
+def build_dataset(reboot=False):
+    """Builds the database of gender based on Wikipedia search.
+
+    This code takes a list of first names separated by a line break \n
+    in file Names.txt and constructs a database in NamesOut.txt by assigning
+    them a gender.
+
+    NamesOut.txt will contain the same names, but followed by the
+    assigned gender (both of them being seprated by a tab \t).
+
+    The gender attribution (M, F, UNI, INI or UNK) is done according to the
+    occurrence of first names on Wikipedia pages concerning them.
+    M: male
+    F: female
+    UNI: unisex
+    INI: initials
+    UNK: unknown
+
+    If NamesOut.txt already exists, it will be ignored and overwritten.
+    Information on gender assignment is present in the log file (NamesLog.txt).
+    The log file is automatically detected to launch the code back where it was
+    in the case it got interrupted.
+
+    Set reboot=True if you want to disregard log files and start from scratch
+    """
 
     cwd = Path(__file__).parent.absolute()
     inputnames = cwd / 'Names.txt'
@@ -314,7 +333,7 @@ if __name__ == '__main__':
     print('Names treatment')
     # Keeping only names that are not in log file in namesfil
     namesfil = []
-    i = 0
+
     for name in namestot:
         if index(datanames, name) == -1:
             namesfil.append(name)
@@ -342,3 +361,249 @@ if __name__ == '__main__':
         fileout.write('\n'.join([name + '\t' + gender_data[name]
                                  for name in namestot_raw]))
     print('Done')
+
+
+def namecleanWoSall(prenom):
+    """Cleans a first name string and separates it into a list of strings,
+    ordered by priority, to analyze for Wiki-Gendersort.
+
+    This is a special modification to use on specific names format in the
+    Web of Science database. Normal names should use nameclean().
+    """
+
+    name = prenom
+    # Takes words in quotations and parenthesis as a penultimate priority
+    while (('(' in name) and (')' in name) or
+           ('"' in name and name.find('"') != name.rfind('"'))):
+        if (name.find('(') < name.rfind(')') and
+                ('(' in name) and (')' in name)):
+            name = (name[:name.find('(')] + ' ' +
+                    name[name.rfind(')')+1:] + ' ' +
+                    name[name.find('(')+1:name.rfind(')')])
+        elif ('"' in name and name.find('"') != name.rfind('"')):
+            name = (name[:name.find('"')] + ' ' +
+                    name[name.rfind('"')+1:] + ' ' +
+                    name[name.find('"')+1:name.rfind('"')])
+        else:
+            break
+    # Cleans strings of the list: Gets rid of strings without letters,
+    # separates fused strings and gets rid of periods at the end of strings
+    # while making the latter last in priority
+    namf = name.replace('-', ' ').replace('_', ' ').split()
+    j = 0
+    while j < len(namf):
+        if countalpha(namf[j]) == 0:
+            del namf[j]
+            j -= 1
+        j += 1
+    j = 0
+    while j < len(namf):
+        if len(namf[j]) >= 4:
+            if (namf[j][-1] == '.' and namf[j][-2].isupper() and
+                    namf[j][-3].islower()):
+                namf.insert(j+1, namf[j][-2:])
+                namf[j] = namf[j][:-2]
+        if namf[j][-1] == '.' and len(namf[j]) > 1:
+            namf.append(namf[j][:-1])
+            del namf[j]
+            j -= 1
+        j += 1
+
+    # Takes the strings that are not initials (if 1 letter or no vowels)
+    namf2 = []
+    for nam in namf:
+        if countalpha(nam) > 1 and countvowel(nam) > 0:
+            if len(nam) <= 1:
+                namf2.append(nam.upper())
+            else:
+                namf2.append(nam[0].upper()+nam[1:].lower())
+    return(namf2)
+
+
+def nameclean(prenom):
+    """Cleans a first name string and separates it into a list of strings,
+    ordered by priority, to analyze for Wiki-Gendersort.
+    """
+
+    name = prenom
+    # Takes words in quotations and parenthesis as a penultimate priority
+    while (('(' in name) and (')' in name) or
+           ('"' in name and name.find('"') != name.rfind('"'))):
+        if (name.find('(') < name.rfind(')') and
+                ('(' in name) and (')' in name)):
+            name = (name[:name.find('(')] + ' ' +
+                    name[name.rfind(')')+1:] + ' ' +
+                    name[name.find('(')+1:name.rfind(')')])
+        elif ('"' in name and name.find('"') != name.rfind('"')):
+            name = (name[:name.find('"')] + ' ' +
+                    name[name.rfind('"')+1:] + ' ' +
+                    name[name.find('"')+1:name.rfind('"')])
+        else:
+            break
+
+    # Cleans strings of the list: Gets rid of strings without letters,
+    # separates fused strings and gets rid of periods at the end of strings
+    # while making the latter last in priority (AliM. -> Ali M)
+    namf = ''
+    for i in name:
+        if countalpha(i) == 1 or i == '.':
+            namf += i
+        else:
+            namf += ' '
+    namf = namf.split()
+
+    j = 0
+    while j < len(namf):
+        if countalpha(namf[j]) == 0:
+            del namf[j]
+            j -= 1
+        j += 1
+    j = 0
+    while j < len(namf):
+        if len(namf[j]) >= 4:
+            if (namf[j][-1] == '.' and namf[j][-2].isupper() and
+                    namf[j][-3].islower()):
+                namf.insert(j+1, namf[j][-2:])
+                namf[j] = namf[j][:-2]
+        if namf[j][-1] == '.' and len(namf[j]) > 1:
+            namf.append(namf[j][:-1])
+            del namf[j]
+            j -= 1
+        j += 1
+
+    # Resplit the periods (A.Carl -> A Carl)
+    j = 0
+    while j < len(namf):
+        namsplit = namf[j].split('.')
+        if len(namsplit) > 1:
+            del namf[j]
+            for k in range(len(namsplit)):
+                namf.insert(j+k, namsplit[k])
+            j += len(namsplit)-1
+        j += 1
+
+    # Takes the strings that are not initials (if 1 letter or no vowels)
+    namf2 = []
+    for nam in namf:
+        if countalpha(nam) > 1 and countvowel(nam) > 0:
+            if len(nam) <= 1:
+                namf2.append(nam.upper())
+            else:
+                namf2.append(nam[0].upper()+nam[1:].lower())
+    return(namf2)
+
+
+class wiki_gendersort():
+    def __init__(self,
+                 input_path=None):
+        if input_path is None:
+            cwd = Path(__file__).parent.absolute()
+            self.input_path = cwd / 'NamesOut.txt'
+        else:
+            self.input_path = Path(input_path)
+
+        print('Importing names database from ' + self.input_path.name)
+        self.names_key = {}
+        with open(self.input_path, 'r', encoding='utf-8') as filewg:
+            for line in filewg.readlines():
+                ls = line.replace('\n', '').split('\t')
+                name = '\t'.join(ls[0:-1]).upper()
+                gend = ls[-1]
+                self.names_key[name] = gend
+
+    def assign(self,
+               name):
+        "Assign a gender to a first name (string)"
+        self.unknown_set = []
+        namelist = nameclean(name)
+        gend = 'NULL'
+        if len(namelist) == 0:
+            gend = 'INI'
+        j = 0
+        while (gend == 'UNK' or gend == 'NULL') and j < len(namelist):
+            if namelist[j].upper() in self.names_key:
+                gend = self.names_key[namelist[j].upper()]
+            elif unidecode(namelist[j].upper()) in self.names_key:
+                gend = self.names_key[unidecode(namelist[j].upper())]
+            else:
+                newname = namelist[j][0].upper() + namelist[j][1:].lower()
+                self.unknown_set.append(newname)
+                if namelist[j].upper() != unidecode(namelist[j].upper()):
+                    newname = (unidecode(namelist[j].upper())[0] +
+                               unidecode(namelist[j].upper())[1:].lower())
+                    self.unknown_set.append(newname)
+            j += 1
+        if name.upper() == 'NULL' or len(name) == 0:
+            gend = 'UNK'
+
+        return gend
+
+    def file_assign(self,
+                    input_path,
+                    output_path=None,
+                    unknown_path=None):
+        """Assigns a gender to a list of first names in a file.
+
+        Parameters
+        ----------
+        input_path: str
+            path to the file containing names to assign a gender.
+            The names should be separated by a line break in a .txt file
+            encoded in utf-8.
+
+        output_path: str, optional
+            path to the file containing the genders of the names.
+            Each entry will be separated by a line break \n, and the gender
+            will be separated from the name by a tab \t, encoded in utf-8.
+
+            If None, the path will be input_path+'_genders.txt'.
+            Default is None.
+
+        unknown_path: str, optional
+            path to the file containing names that were not in the database
+            and were attributed 'UNK' by default.
+            Each entry will be separated by a line break \n. Those names
+            could be added to Names.txt, and then the database can be updated
+            by running build_dataset(), assuming you still have the log files.
+            Alternatively, the unknown names could be manually assigned to a
+            gender with name_to_gender().
+
+            If None, the path will be input_path+'_unknown.txt'.
+            Default is None.
+        """
+        input_path = Path(input_path).absolute()
+        print('Assigning gender to the names in file ' + input_path.name)
+        if output_path is None:
+            output_path = input_path.parent / (input_path.stem+'_output.txt')
+        if unknown_path is None:
+            unknown_path = input_path.parent / (input_path.stem+'_unknown.txt')
+        infile = open(input_path, 'r', encoding='utf-8')
+        outfile = open(output_path, 'w', encoding='utf-8')
+        newnames = []
+        for line in infile.readlines():
+            name = line.replace('\n', '')
+            gend = self.assign(name)
+            for newname in self.unknown_set:
+                nind = bisect_left(newnames, newname)
+                if nind == len(newnames) or newnames[nind] != newname:
+                    newnames.insert(nind, newname)
+            outfile.write(line.replace('\n', '') + '\t' + gend + '\n')
+        infile.close()
+        outfile.close()
+        print('Genders assigned in file ' + output_path.name)
+        if len(newnames) != 0:
+            with open(unknown_path, 'w', encoding='utf-8') as newfile:
+                newfile.write('\n'.join(newnames))
+            print('%i unknown names identified in file ' % len(newnames) +
+                  unknown_path.name)
+            print('Consider adding those names to Names.txt and ' +
+                  'running build_dataset()')
+
+
+if __name__ == '__main__':
+    # build_dataset()
+
+    # WG = wiki_gendersort()
+    # WG.assign('Nicolas')
+    # WG.file_assign('test_file.txt')
+    pass
